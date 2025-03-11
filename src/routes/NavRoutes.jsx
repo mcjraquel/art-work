1c6b0bfad10030d19852ddeda2497d06db093f0b
mcjraquel <mcjraquel@gmail.com>
2025-03-11 10:46:26 +0800

    fix: Remove cart and checkout from routes

--- src/routes/NavRoutes.jsx
+++ src/routes/NavRoutes.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Home } from "../pages/Home/Home";
-import { Cart } from "../pages/Cart/Cart";
 import { Route, Routes } from "react-router-dom";
 import { Login } from "../pages/auth/Login/Login";
 import { ProductListing } from "../pages/ProductListing/ProductListing";
@@ -8,7 +7,6 @@ import { ProductDetails } from "../pages/ProductDetails/ProductDetails";
 import { RequiresAuth } from "../components/requires-auth/RequiresAuth";
 import { Signup } from "../pages/auth/Signup/Signup";
 import { Logout } from "../pages/auth/Logout/Logout";
-import { Checkout } from "../pages/Checkout/Checkout";
 import { Wishlist } from "../pages/Wishlist/Wishlist";
 import { UserProfile } from "../pages/UserProfile/UserProfile";
 import { Profile } from "../pages/UserProfile/Profile/Profile";
@@ -20,14 +18,6 @@ export const NavRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route
-        path="/cart"
-        element={
-          <RequiresAuth>
-            <Cart />
-          </RequiresAuth>
-        }
-      />
       <Route
         path="/wishlist"
         element={
@@ -42,14 +32,6 @@ export const NavRoutes = () => {
       <Route path="/product-listing" element={<ProductListing />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/product-details/:productId" element={<ProductDetails />} />
-      <Route
-        path="/checkout"
-        element={
-          <RequiresAuth>
-            <Checkout />
-          </RequiresAuth>
-        }
-      />
       <Route path="/profile" element={<UserProfile />}>
         <Route
           path="/profile/"
